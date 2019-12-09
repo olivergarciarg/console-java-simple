36d62f605e56d8fd57ff0471b3ec9e69bee2bcd6
Oliver Garcia <oojj@yahoo.com>
2019-12-09 14:00:42 +0000

    z

--- src/main/java/org/eclipse/che/examples/Test.java
+++ src/main/java/org/eclipse/che/examples/Test.java
@@ -22,4 +22,51 @@ public class Test {
     public static boolean hasTeen(int n1, int n2, int n3){
         return (isTeen(n1)||isTeen(n2)||isTeen(n3));
     }
+
+    public static double area (double radius){
+        double areaCircle = -1;
+        if (radius>=0){
+            areaCircle = radius * radius * Math.PI;
+        }
+        return areaCircle;
+    }
+
+    public static double area (double x, double y){
+        double areaSquare = -1;
+        if (x>=0 && y>=0){
+            areaSquare = x * y;
+        }
+        return areaSquare;
+    }
+
+    public static void printYearsAndDays(long minutes){
+        if (minutes < 0){
+            System.out.println("Invalid Value");
+        } else {
+            long days = (long) minutes / (24 * 60);
+            long years = (long) days / (365);
+            days = (long) days % (365);
+            System.out.println(minutes + " min = " + years + " y and " + days + " d");
+        }
+    }
+
+    public static void printEqual(int p1, int p2, int p3){
+        if (p1 < 0 || p2<0 || p3<0){
+            System.out.println("Invalid Value");
+        } else if ((p1 == p2) && (p1 == p3) && (p2 == p3)){
+            System.out.println("All numbers are equal");
+        } else if ((p1 != p2) && (p1 != p3) && (p2 != p3)){
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different");
+        }
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature){
+        boolean catPlaying = false;
+        if (temperature >= 25 && (temperature <= 35 || (temperature <= 45 && summer))){
+            catPlaying =  true;
+        } 
+        return catPlaying;
+    }
 }
