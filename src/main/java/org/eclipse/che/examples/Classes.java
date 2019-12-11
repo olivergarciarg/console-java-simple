e9ae5269906a5b93d88307bf00a06f73be6e5c9c
Oliver Garcia <oojj@yahoo.com>
2019-12-11 13:20:21 +0000

    z

---
+++ src/main/java/org/eclipse/che/examples/Classes.java
@@ -0,0 +1,8 @@
+package org.eclipse.che.examples;
+
+public class Classes {
+    public static void main(String... argvs) {
+        String a = "Che";
+        System.out.println("Hello World " + a + "!");
+    }
+}
