0b6112ea861df3d4c0bac53633d81cf7ebdcb826
Oliver Garcia <oojj@yahoo.com>
2019-12-12 13:37:41 +0000

    z

--- src/main/java/org/eclipse/che/examples/Point.java
+++ src/main/java/org/eclipse/che/examples/Point.java
@@ -1,9 +1,16 @@
-package org.eclipse.che.examples;
-
 public class Point {
-
+    // write your code here
     private int x;
     private int y;
+
+    public Point(){
+        this(0,0);
+    }
+
+    public Point(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
 
     public int getX(){
         return this.x;
@@ -21,7 +28,17 @@ public class Point {
         this.y=y;
     }
 
-    public double getArea(){
-        return this.height * this.width;
+    public double distance(int x, int y){
+        int newX = this.x - x;
+        int newY = this.y - y;
+        return Math.sqrt((newX * newX) + (newY * newY));
+    }
+
+    public double distance(){
+        return distance(0,0);
+    }
+
+    public double distance(Point secondPoint){
+        return distance(secondPoint.x, secondPoint.y);
     }
 }
