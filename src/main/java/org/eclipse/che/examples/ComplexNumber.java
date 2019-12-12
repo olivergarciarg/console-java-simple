0538e3c557d3d8cff0e28115b27a23d3c7ca326c
Oliver Garcia <oojj@yahoo.com>
2019-12-12 14:09:43 +0000

    z

---
+++ src/main/java/org/eclipse/che/examples/ComplexNumber.java
@@ -0,0 +1,38 @@
+package org.eclipse.che.examples;
+
+public class ComplexNumber {
+
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary){
+        this.real=real;
+        this.imaginary=imaginary;
+    }
+
+    public double getReal(){
+        return this.real;
+    }
+
+    public double getImaginary(){
+        return this.imaginary;
+    }
+
+    public void add(double real, double imaginary){
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber complexNumber){
+        add(complexNumber.real, complexNumber.imaginary);
+    }
+
+    public void subtract(double real, double imaginary){
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber complexNumber){
+        subtract(complexNumber.real, complexNumber.imaginary);
+    }
+}
